985f6034e51fbfd8bb2b88f0626784d2dd4832f3
David Ortiz <davidhlanz@gmail.com>
2025-10-14 13:35:45 +0200

    condiciones ejercicio3

---
+++ condiciones/ejercicio3.py
@@ -0,0 +1,7 @@
+PrimerNumero = int(input("Primer numero: "))
+SegundoNumero = int(input("Segundo numero: "))
+
+if SegundoNumero == 0:
+    print("No se puede dividir entre 0")
+else:
+    print(PrimerNumero / SegundoNumero)
