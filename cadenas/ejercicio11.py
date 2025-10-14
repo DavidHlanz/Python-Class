b9e08c135ae1a3ab41976a186fb17b6442ff9cca
David Ortiz <davidhlanz@gmail.com>
2025-10-14 13:23:01 +0200

    Condiciones primer ejercicio

---
+++ cadenas/ejercicio11.py
@@ -0,0 +1,8 @@
+nombre = input("Introduce el nombre del producto: ")
+precio = float(input("Introduce el precio unitario: "))
+unidades = int(input("Introduce el número de unidades: "))
+
+total = precio * unidades
+
+
+print(f"{nombre}: {precio:04.2f} euros {uds:02d} unidades y el total: {total:05.2f} euros ")
