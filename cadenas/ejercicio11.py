18c1301da77c458b147e53763e0151b27d7d68fe
David Ortiz <davidhlanz@gmail.com>
2025-10-14 13:24:08 +0200

    old fix

--- cadenas/ejercicio11.py
+++ cadenas/ejercicio11.py
@@ -5,4 +5,4 @@ unidades = int(input("Introduce el número de unidades: "))
 total = precio * unidades
 
 
-print(f"{nombre}: {precio:04.2f} euros {uds:02d} unidades y el total: {total:05.2f} euros ")
+print(f"{nombre}: {precio:04.2f} euros {unidades:02d} unidades y el total: {total:05.2f} euros ")
