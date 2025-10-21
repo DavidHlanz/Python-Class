f5a90228372b6d10848dbac5c8d4beed2f4c2733
David Ortiz <davidhlanz@gmail.com>
2025-10-21 13:39:54 +0200

    condiciones ejercicio 8

---
+++ condiciones/ejercicio8.py
@@ -0,0 +1,7 @@
+puntuacion = float(input("Cual es tu puntuacion: "))
+if (puntuacion == 0.0):
+    print(f"Dinero ganado " +str(2400.00 * puntuacion))
+elif puntuacion == 0.4:
+    print(f"Dinero ganado " +str(2400.00 * puntuacion))
+elif puntuacion == 0.6 or puntuacion > 0.6:
+    print(f"Dinero ganado " +str(2400.00 * puntuacion))
