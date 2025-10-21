5be37e8b71744f0bff3309faebbe669400bc9eca
David Ortiz <davidhlanz@gmail.com>
2025-10-21 14:28:52 +0200

    update condiciones ejercicio10

--- condiciones/ejercicio10.py
+++ condiciones/ejercicio10.py
@@ -2,9 +2,9 @@ tipo = input("¿Quieres una pizza vegetariana? (sí/no): ")
 
 if tipo == "si":
     ingredientev = input("Elige un ingrediente (pimiento o tofu): ")
-    print("Elegiste vegetariana con: " + ingredientev)
+    print("Elegiste vegetariana con tomate, mozzalella y " + ingredientev)
 else:
     ingrediente = input("Elige un ingrediente (peperoni, jamón o salmón): ").lower()
-    print("Elegiste no vegetariana con: " + ingrediente)
+    print("Elegiste no vegetariana con tomate, mozzalella y " + ingrediente)
 
 
